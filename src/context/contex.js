ba05faa1f038de28294ceab7303d5ffa1fe5328c
PATEL MOHIT <mohitptl183@gmail.com>
2024-09-05 12:04:14 +0530

    Add 2

--- src/context/contex.js
+++ src/context/contex.js
@@ -1,7 +1,7 @@
 export const USER_API_END_POINT = 'https://mern-backend-ti7y.onrender.com/api/v1/user';
 export const BLACKSTU_API_END_POINT = 'https://mern-backend-ti7y.onrender.com/api/v1/senseistu';
 export const GALLERY_API_END_POINT = 'https://mern-backend-ti7y.onrender.com/api/images';
-export const CONTACTUS_API_END_POINT = 'https://mern-backend-ti7y.onrender.com/api/images';
+export const CONTACTUS_API_END_POINT = 'https://mern-backend-ti7y.onrender.com/api/contactus';
 
 // https://mern-backend-ti7y.onrender.com
 // http://localhost:4000
