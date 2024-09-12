699e1ad04cfcd748806167b60af40bad8fc9bc86
PATEL MOHIT <mohitptl183@gmail.com>
2024-09-12 11:32:57 +0530

    update Header ui

--- src/components/Navbar/BlackBeltStu.jsx
+++ src/components/Navbar/BlackBeltStu.jsx
@@ -52,18 +52,18 @@ function BlackBeltStu() {
 
   return (
     <div className="max-w-7xl mx-auto bg-slate-900 border border-gray-400 rounded-2xl my-5 p-8">
-      <p className="text-[4vw] pb-3 lg:text-[2vw] font-bold text-yellow-300">
+      <p className="text-[4vw] pb-3 sm:text-[2vw] font-bold text-yellow-300">
         BlackBelt Students of Sensei <span className="font-serif text-yellow-400 ">{user.fullname}</span> 
       </p>
       <div className="overflow-x-auto">
         <table className="w-full text-left">
           <thead>
             <tr className="bg-slate-800 text-violet-600">
-              <th className="w-[20%] text-[3.5vw] lg:text-[1.6vw] font-bold text-2xl">Date</th>
-              <th className="w-[55%] text-[3.5vw] lg:text-[1.6vw] font-bold text-2xl">Name</th>
-              <th className="w-[25%] text-[3.5vw] lg:text-[1.6vw] font-bold text-2xl">Certificate</th>
+              <th className="w-[20%] text-[3.5vw] sm:text-[1.6vw] font-bold text-2xl">Date</th>
+              <th className="w-[55%] text-[3.5vw] sm:text-[1.6vw] font-bold text-2xl">Name</th>
+              <th className="w-[25%] text-[3.5vw] sm:text-[1.6vw] font-bold text-2xl">Certificate</th>
               {user && user.role === "Owner" && (
-                <th className="w-[10%] text-[3.5vw] lg:text-[1.6vw] font-bold text-2xl">Action</th>
+                <th className="w-[10%] text-[3.5vw] sm:text-[1.6vw] font-bold text-2xl">Action</th>
               )}
             </tr>
           </thead>
@@ -71,15 +71,15 @@ function BlackBeltStu() {
             {blackStudent.length > 0 ? (
               blackStudent.map((stu, index) => (
                 <tr className="hover:bg-slate-800" key={stu._id || index}>
-                  <td className="text-sky-100 text-[2.5vw] lg:text-[1.2vw]">
+                  <td className="text-sky-100 text-[2.5vw] sm:text-[1.2vw]">
                     {new Date(stu.date).toLocaleDateString()}
                   </td>
-                  <td className="text-[3vw] studentname lg:text-[1.3vw]">
+                  <td className="text-[3vw] studentname sm:text-[1.3vw]">
                     {stu.studentname}
                   </td>
                   <td>
                     <a
-                      className="text-blue-500 text-[2.9vw] lg:text-[1.2vw] underline"
+                      className="text-blue-500 text-[2.9vw] sm:text-[1.2vw] underline"
                       href={stu.certificate}
                       target="_blank"
                       rel="noopener noreferrer"
@@ -94,7 +94,7 @@ function BlackBeltStu() {
                         className="p-[0.3vw] bg-red-600 bg-opacity-50 text-white rounded"
                       >
                         <LucideShieldClose
-                          className="w-[3.5vw] h-[3.5vw] lg:w-[1vw] lg:h-[1vw]"
+                          className="w-[3.5vw] h-[3.5vw] sm:w-[1vw] sm:h-[1vw]"
                         />
                       </button>
                     </td>
--- src/components/Navbar/Header.jsx
+++ src/components/Navbar/Header.jsx
@@ -49,8 +49,8 @@ function Header() {
 
   return (
     <header className="bg-gray-800 text-white">
-      <div className="flex flex-col lg:flex-row items-center justify-between px-[1.5vw] py-4">
-        <div className="lg:hidden flex items-center mb-4 lg:mb-0">
+      <div className="flex flex-col sm:flex-row items-center justify-between px-[1.5vw] py-4">
+        <div className="sm:hidden flex items-center mb-4 sm:mb-0">
           <NavLink to="/" className="text-white">
             <img
               src={logo}
@@ -59,37 +59,97 @@ function Header() {
             />
           </NavLink>
         </div>
-        <div className="hidden lg:flex items-center">
+        <div className="hidden sm:flex items-center">
           <NavLink to="/" className="text-white">
             <img
               src={logo}
               alt="Martial Arts Logo"
-              className="h-16 lg:h-[8vw] lg:w-[8.8vw] rounded-full inline"
+              className="h-16 sm:h-[8vw] sm:w-[8.8vw] rounded-full inline"
             />
           </NavLink>
         </div>
         <div className="text-center w-full">
-          <h1 className="lg:text-[2vw] text-[5vw] font-bold">
+          <h1 className="sm:text-[2vw] text-[5vw] font-bold">
             ABX ASSOCIATION - INDIA
           </h1>
-          <h2 className="lg:text-[1vw] text-[2.5vw] font-mono">
+          <h2 className="sm:text-[1vw] text-[2.5vw] font-mono">
             Affiliated With:  SHUUGOUKAI 
           </h2>
         </div>
       </div>
 
       <div className="container mx-auto flex justify-between items-center px-[1.5vw]">
-        <nav className="lg:flex flex-wrap lg:ml-auto w-full lg:w-auto">
-          <div className="lg:hidden flex justify-between">
+        <nav className="sm:flex flex-wrap sm:ml-auto w-full sm:w-auto">
+          <div className="sm:hidden flex justify-between">
             <img
               src={punch}
               alt="Toggle navigation"
               onClick={toggleNavVisibility}
               className="cursor-pointer h-[11vw] w-[11vw] rounded-full"
             />
+              <div className=" text-wrap items-center">
+                {!user? (
+                  <div className="items-center sm:-mt-2 mt-0">
+                    <NavLink to="/login">
+                      <Button className="w-[15vw] text-wrap sm:w-[6vw] bg-slate-50 text-black hover:bg-slate-200">
+                        Login
+                      </Button>
+                    </NavLink>
+                    <NavLink to="/signup">
+                      <Button className="w-[15vw] text-wrap sm:w-[6vw]">
+                        SignUp
+                      </Button>
+                    </NavLink>
+                  </div>
+                ) : (
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Avatar className="cursor-pointer">
+                        <AvatarImage src={user.photo} />
+                      </Avatar>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-80">
+                      <div className="flex gap-10 ">
+                        <Avatar className="cursor-pointer">
+                          <AvatarImage src={user.photo} />
+                        </Avatar>
+                        <div>
+                          <h1 className="font-medium">{user.fullname}</h1>
+                          <h1 className="font-medium text-gray-500">
+                            {user.role}
+                          </h1>
+                        </div>
+                      </div>
+                      <div className="flex flex-col ">
+                        <div className="flex items-center">
+                          <User2 />
+                          <NavLink to="/profile">
+                            <Button
+                              variant="link"
+                              className="font-medium text-gray-500"
+                            >
+                              View Profile
+                            </Button>
+                          </NavLink>
+                        </div>
+                        <div className="flex items-center">
+                          <LogOut />
+                          <Button
+                            onClick={logoutHandler}
+                            variant="link"
+                            className="font-medium text-gray-500"
+                          >
+                            Logout
+                          </Button>
+                        </div>
+                      </div>
+                    </PopoverContent>
+                  </Popover>
+                )}
+              </div>
           </div>
           {isNavVisible && (
-            <ul className="flex flex-col lg:flex-row lg:flex-wrap text-[5vw] lg:text-[1.4vw] ml-[1vw] lg:space-x-8">
+            <ul className="flex flex-col sm:flex-row sm:flex-wrap text-[5vw] sm:text-[1.4vw] ml-[1vw] sm:space-x-8">
               <li>
                 <NavLink
                   to="/"
@@ -150,16 +210,16 @@ function Header() {
                   Contact
                 </NavLink>
               </li>
-              <li className="items-center">
+              <li className="sm:flex hidden  items-center">
                 {!user ? (
-                  <div className="items-center lg:-mt-2 mt-0">
+                  <div className="items-center sm:-mt-2 mt-0">
                     <NavLink to="/login">
-                      <Button className="w-[15vw] text-wrap lg:w-[6vw] bg-slate-50 text-black hover:bg-slate-200">
+                      <Button className="w-[15vw] text-wrap sm:w-[6vw] bg-slate-50 text-black hover:bg-slate-200">
                         Login
                       </Button>
                     </NavLink>
                     <NavLink to="/signup">
-                      <Button className="w-[15vw] text-wrap lg:w-[6vw]">
+                      <Button className="w-[15vw] text-wrap sm:w-[6vw]">
                         SignUp
                       </Button>
                     </NavLink>
